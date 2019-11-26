f6c927a6fb550ca780d35d0621dd654b365f1a20
Sungsu <h2s0109@gmail.com>
2019-11-26 15:40:43 +0900

    17차 xml 문제 해결

--- SCopy/XMLCompiler.py
+++ SCopy/XMLCompiler.py
@@ -26,6 +26,7 @@ def parsing_includeTasking(xmlpath, include_path):
     tree = ET.parse(xmlpath)
     TaskingRoot = tree.getroot()
     print(TaskingRoot.tag)
+    find_option = False
     for option in TaskingRoot.iter('option'):
         if option.attrib['superClass'] == "com.tasking.ctc.cc.includePaths":
             for value in option.findall('listOptionValue'):
@@ -35,6 +36,23 @@ def parsing_includeTasking(xmlpath, include_path):
                 # tmp_path = "&quot;" + tmp_path + "&quot;"
                 ET.SubElement(option, 'listOptionValue', builtIn="false",
                               value=tmp_path)
+            find_option = True
+    if find_option == False:
+        for option in TaskingRoot.iter('tool'):
+            if option.attrib['superClass'] == "com.tasking.ctc.cc.abs.debug":
+                for option2 in option.iter('option'):
+                    if option2.attrib['superClass'] == "com.tasking.ctc.cc.pr36858":
+                        ET.SubElement(option, 'option', id = 'com.tasking.ctc.cc.includePaths.552904157', name="Include paths", superClass="com.tasking.ctc.cc.includePaths", valueType="includePath")
+                        break
+        for option in TaskingRoot.iter('option'):
+            if option.attrib['superClass'] == "com.tasking.ctc.cc.includePaths":
+                for value in option.findall('listOptionValue'):
+                    print(value.attrib['value'])
+                    option.remove(value)
+                for tmp_path in include_path:
+                    # tmp_path = "&quot;" + tmp_path + "&quot;"
+                    ET.SubElement(option, 'listOptionValue', builtIn="false",
+                                value=tmp_path)
     indent(TaskingRoot)
     tree.write(xmlpath, encoding="utf-8", xml_declaration=True)
     return
